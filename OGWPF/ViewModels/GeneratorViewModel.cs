e237ec17a14c93e8a1419690ed24d515e878ad9a
tim-dm <imvu_data@zoho.com>
2020-07-21 01:35:48 -0700

    Removed unused using statements

--- OGWPF/ViewModels/GeneratorViewModel.cs
+++ OGWPF/ViewModels/GeneratorViewModel.cs
@@ -1,16 +1,8 @@
 ﻿using OpacEngine;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace OGWPF.ViewModels
 {
